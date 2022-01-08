fb5861f411117b2efac2ca1952702b42b866d74c
nihal kudligi <nihal1011ML@gmail.com>
2022-01-08 23:48:47 +0530

    added benchmark for random shorturl generator

--- utility/utility_test.go
+++ utility/utility_test.go
@@ -10,3 +10,9 @@ func TestGetRandomShortUrl(t *testing.T){
   a := GetRandomShortUrl()
   assert.NotNil(t, a)
 }
+
+func BenchmarkGetRandomShortUrl(b *testing.B){
+  for i := 0; i < b.N; i++ {
+        GetRandomShortUrl()
+  }
+}
