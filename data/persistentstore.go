8e28c41507c50ef65699a79d1f4ea6a392fce5ca
nihal kudligi <nihal1011ML@gmail.com>
2022-01-09 20:03:36 +0530

    added weak persistence

---
+++ data/persistentstore.go
@@ -0,0 +1,92 @@
+package data
+
+import (
+  "sync"
+  "fmt"
+  "encoding/json"
+  "os"
+  "io/ioutil"
+)
+
+type EventuallyPersistentDataStorage struct {
+  Long2Short map[string]string
+  Short2Long map[string]string
+  Mu *sync.RWMutex
+  PersistSignalChannel chan bool
+}
+
+var (
+    channelCounter = 0
+)
+
+
+func (d *EventuallyPersistentDataStorage) CheckGetVal4LongUrl(longUrl string) (string, bool){
+  d.Mu.RLock();
+  shortUrl, ok := d.Long2Short[longUrl]
+  d.Mu.RUnlock();
+  return shortUrl, ok
+}
+
+func (d *EventuallyPersistentDataStorage) CheckGetVal4ShortUrl(shortUrl string) (string, bool){
+  d.Mu.RLock();
+  longUrl, ok := d.Short2Long[shortUrl]
+  d.Mu.RUnlock();
+  return longUrl, ok
+}
+
+func (d *EventuallyPersistentDataStorage) InsertUrlPair(longUrl string,shortUrl string) (string, bool){
+  d.Mu.Lock();
+  d.Long2Short[longUrl] = shortUrl
+  d.Short2Long[shortUrl] = longUrl
+  d.Mu.Unlock();
+  d.PersistSignalChannel <- true
+  return "", true
+}
+
+func (d *EventuallyPersistentDataStorage) LogAll(){
+  for key, value := range d.Long2Short {
+    fmt.Println(key, value)
+  }
+  fmt.Println(len(d.Long2Short), len(d.Short2Long))
+}
+
+func (d *EventuallyPersistentDataStorage) PersistMap(){
+  for _ = range d.PersistSignalChannel {
+    if channelCounter % 100 == 99 {
+        d.Mu.RLock()
+        writeToFile(d.Short2Long, "./persist/backupS2L.json")
+        writeToFile(d.Long2Short, "./persist/backupL2S.json")
+        d.Mu.RUnlock();
+    }
+    channelCounter += 1
+  }
+}
+
+func (d *EventuallyPersistentDataStorage) LoadCache(){
+  jsonFile, err := os.Open("./persist/backupS2L.json")
+  if err != nil {
+      fmt.Println("No persisted store")
+      return
+  }
+  byteValue, _ := ioutil.ReadAll(jsonFile)
+  json.Unmarshal(byteValue, &d.Short2Long)
+
+  jsonFile, err = os.Open("./persist/backupL2S.json")
+  if err != nil {
+      fmt.Println("No persisted store")
+      return
+  }
+  byteValue, _ = ioutil.ReadAll(jsonFile)
+  json.Unmarshal(byteValue, &d.Long2Short)
+}
+
+func writeToFile(cache map[string]string, filePath string)(){
+  b, err := json.Marshal(cache)
+  f, err := os.Create(filePath)
+  if err != nil {
+      fmt.Println(err)
+      return
+  }
+  l, _ := f.WriteString(string(b))
+  fmt.Println(l, "bytes written successfully to ", filePath)
+}
--- main.go
+++ main.go
@@ -15,8 +15,11 @@ func main() {
 	r := mux.NewRouter()
 	// store := data.InMemoryDataStoreV2{new(sync.Map), new(sync.Map)}
 	// store := data.InMemoryDataStoreV2Plain{make(map[string]string), make(map[string]string)}
-	store := data.InMemoryDataStoreV2RWMutex{make(map[string]string), make(map[string]string), new(sync.RWMutex)}
-  service := data.DataServiceV2{&store, utility.GetRandomShortUrl}
+	// store := data.InMemoryDataStoreV2RWMutex{make(map[string]string), make(map[string]string), new(sync.RWMutex)}
+	store := data.EventuallyPersistentDataStorage{make(map[string]string), make(map[string]string), new(sync.RWMutex), make(chan bool)}
+	store.LoadCache()
+
+	service := data.DataServiceV2{&store, utility.GetRandomShortUrl}
 
 	router := &api.Router{service}
 
@@ -25,7 +28,7 @@ func main() {
   r.HandleFunc("/lengthen", router.LengthenUrl).Methods("POST")
 	r.HandleFunc("/{shortUrl}", router.Redirect).Methods("GET")
 	r.HandleFunc("/", router.LogAll).Methods("GET")
-
+	go store.PersistMap()
 	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 
   http.ListenAndServe(":9090", loggedRouter)
