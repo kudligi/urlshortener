d8f40d1ea9af6bac070342634ab694a0cbd64048
nihal kudligi <nihal1011ML@gmail.com>
2022-01-09 12:00:49 +0530

    seeding rand with cryptographically secure seed

--- utility/utility.go
+++ utility/utility.go
@@ -2,12 +2,23 @@ package utility
 
 import (
   "github.com/btcsuite/btcutil/base58"
-  "math/rand"
+  crypto_rand "crypto/rand"
+  "encoding/binary"
+  math_rand "math/rand"
 )
+
+func init(){
+  var b [8]byte
+    _, err := crypto_rand.Read(b[:])
+    if err != nil {
+        panic("cannot seed math/rand package with cryptographically secure random number generator")
+    }
+    math_rand.Seed(int64(binary.LittleEndian.Uint64(b[:])))
+}
 
 func randomBytes() []byte {
   buf := make([]byte, 8)
-  rand.Read(buf)
+  math_rand.Read(buf)
   return buf
 }
 
